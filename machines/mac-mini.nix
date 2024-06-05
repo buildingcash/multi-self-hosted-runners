822131e4f475e15bab448a8711017647abd609c9
Jasp <jasp@Mac-mini-de-Jasp.local>
2024-06-05 18:23:20 +0200

    add nix configuration

---
+++ machines/mac-mini.nix
@@ -0,0 +1,19 @@
+# Mac mini setup configuration
+{ pkgs, ... }: {
+
+  # specific packages for mac-mini:
+  environment.systemPackages = [];
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+  # Enable flakes
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;
+
+  system.stateVersion = 4;
+
+  nixpkgs.hostPlatform = "aarch64-darwin";
+}
